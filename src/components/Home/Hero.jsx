205df437db9e0de2dae4d9f52faf38f9fe488ac0
Ridam Chhapiya <ridamchhapiya15@gmail.com>
2025-01-30 17:57:59 +0530

    update

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -5,7 +5,7 @@ import trend from "../../assets/trend.png";
 
 const Hero = () => {
   return (
-    <section className="hero-container">
+    <div className="hero-container">
         <div className="hero-image">
         <img src={HeroImage} alt="Chole Bhature" />
       </div>
@@ -14,12 +14,12 @@ const Hero = () => {
         <img src={trend} alt="trend-logo"/>
          85% would make this again
          </span>
-        <h2>Fluffy Chole Bhature</h2>
+        <h2 >Fluffy Chole Bhature</h2>
         <p>A plate where comfort meets indulgence, and every bite is a celebration of spice, soul, and satisfaction.</p>
         
       </div>
       
-    </section>
+    </div>
   );
 };
 
