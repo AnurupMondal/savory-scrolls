9a92fda46a309f853a0fcaae4ca4561fcef8975d
Anurup Chandra Mondal <anurup.mondal75@gmail.com>
2025-02-02 21:29:20 +0530

    redux working

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Only ONE instance here
+import { Provider } from "react-redux"; // Import Redux Provider
+import { BrowserRouter } from "react-router-dom";
+import store from "./store"; // Import the Redux store
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
